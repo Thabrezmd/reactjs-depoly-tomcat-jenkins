ce1311d7748893625d17a51d4b8a43040e8cf3a4
riti12 <riti@elitemindz.co>
2022-11-08 19:41:37 +0530

    done

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,30 @@
 import { useNavigate, NavLink } from "react-router-dom";
-import '../index.css'
+import "../index.css";
 
-const Home=()=>{
-    const navigate = useNavigate()
+const Home = () => {
+  const navigate = useNavigate();
 
-    return (
-        <>
-        <h1>Welcome Home Page </h1>
-            {/* <button className="btn btn-success" > Register </button> */}
-            <div className="main">
-            <NavLink to={'/register'} className="btn btn-primary">Register</NavLink>
-            </div> 
-            <div className="">
-                <NavLink to={'/binary'} className="btn btn-primary">View</NavLink>
-            </div>
+  return (
+    <>
+      <h1>Welcome Home Page </h1>
+      {/* <button className="btn btn-success" > Register </button> */}
+      <div className="container">
+        <div className="row">
+          <NavLink to={"/register"} className="btn btn-primary">
+            Register
+          </NavLink>
+       
+        <div className="main">
+          <NavLink to={"/binary"} className="btn btn-primary">
+            View
+          </NavLink>
+        </div>
+        </div>
 
-            {/* <button className="btn btn-info" > View </button> */}
-            
-        </>
-    )
-}
+        {/* <button className="btn btn-info" > View </button> */}
+      </div>
+    </>
+  );
+};
 
 export default Home;
