a66bb6eb1251db2d87195ed16b98432df269b3f1
Thabrezmd <116071169+Thabrezmd@users.noreply.github.com>
2023-04-13 12:13:21 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import './App.css';
-//import react, {useEffect, useState} from 'react';
-//import {Routes, Route} from 'react-router-dom'
-//import Home from "./components/home"
-//import Register from './components/form'
-//import Binary from './components/binary'
-//import Navbar from './components/navbar';
-//
-// const Routing=()=>{
- // return(
-   // <>
-     // <Routes>
-     //   <Route path="/" element={<Home/>} />  
-       // <Route path="/register" element={<Register/>} />
-       // <Route path="/binary" element={<Binary/>} />
+import react, {useEffect, useState} from 'react';
+import {Routes, Route} from 'react-router-dom'
+import Home from "./components/home"
+import Register from './components/form'
+import Binary from './components/binary'
+import Navbar from './components/navbar';
 
-     // </Routes>
-   // </>
- // )
-// }
+ const Routing=()=>{
+  return(
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />  
+        <Route path="/register" element={<Register/>} />
+        <Route path="/binary" element={<Binary/>} />
 
-// const App=()=> {
- // const [login, setLogin]= useState("")
+      </Routes>
+    </>
+  )
+ }
 
-  // useEffect(()=>{
-    // let userInf= localStorage.getItem('user')
-  //  userInf= JSON.parse(userInf)
- //   setLogin(userInf)
-// }, [])  
+ const App=()=> {
+  const [login, setLogin]= useState("")
+
+   useEffect(()=>{
+     let userInf= localStorage.getItem('user')
+    userInf= JSON.parse(userInf)
+    setLogin(userInf)
+ }, [])  
 
 
-//  return (
-    // <>
-  //  <div className="App">
-   //   <Navbar/>
-    //  <Routing/>
-   // </div>
-   // </>
-  // );
-// }
+  return (
+     <>
+    <div className="App">
+      <Navbar/>
+      <Routing/>
+    </div>
+    </>
+   );
+ }
 
- // 1export default App;
+  1export default App;
